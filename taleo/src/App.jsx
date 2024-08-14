7716ddd552934ada98ca9b4cf50382e2a02fa860
Mher Barseghyan <mherr.barseghyann@gmail.com>
2024-08-14 16:58:06 +0400

    v1 - story reader

---
+++ taleo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Read from './components/Read'
+
+function App() {
+
+  return (
+    <>
+      <Read/>
+    </>
+  )
+}
+
+export default App
---
+++ taleo/src/components/Read.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "../App.css";
+
+function Read() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const story =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+
+  const wordsPerPage = 126;
+  const page1 = story
+    .split(" ")
+    .slice(
+      currentPage * wordsPerPage,
+      currentPage * wordsPerPage + wordsPerPage
+    )
+    .join(" ");
+  const page2 = story
+    .split(" ")
+    .slice(
+      (currentPage + 1) * wordsPerPage,
+      (currentPage + 1) * wordsPerPage + wordsPerPage
+    )
+    .join(" ");
+
+  function turnLeft() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 2);
+    }
+  }
+
+  function turnRight() {
+    setCurrentPage(currentPage + 2);
+  }
+
+  return (
+    <div className="container">
+      <h2>Current Book</h2>
+      <div className="book">
+        <div className="page">{page1}</div>
+        <div className="page">{page2}</div>
+      </div>
+      <p className="page-number">
+        Page {currentPage + 1} & {currentPage + 2}
+      </p>
+      <div className="buttons">
+        <button onClick={turnLeft} disabled={currentPage <= 0}>
+          &lt;
+        </button>
+        <button onClick={turnRight}>&gt;</button>
+      </div>
+    </div>
+  );
+}
+
+export default Read;
